7527c1855476dd40f541eea7f9f12eadffc10f62
DavidKf <36332661+DavidKf@users.noreply.github.com>
2022-04-09 14:49:15 -0300

    enabling easter egg cypress test

--- cypress/integration/posts.spec.ts
+++ cypress/integration/posts.spec.ts
@@ -1,10 +1,10 @@
 describe('Posts', () => {
-  // it('Space Invaders!', () => {
-  //   cy.visit('/')
-  //   const invaderArray = ['5', '6', '7', '12', '14', '15', '16', '17', '18', '20', '22', '23', '24', '26', '28', '29', '30', '33', '34', '35', '36', '37', '38', '39', '43', '45', '47', '49', '52', '53', '59', '60'];
+  it('Space Invaders!', () => {
+    cy.visit('/')
+    const invaderArray = ['5', '6', '7', '12', '14', '15', '16', '17', '18', '20', '22', '23', '24', '26', '28', '29', '30', '33', '34', '35', '36', '37', '38', '39', '43', '45', '47', '49', '52', '53', '59', '60'];
 
-  //   invaderArray.forEach(block => cy.get(`mat-grid-tile:nth-child(${block})`).click());
-  // });
+    invaderArray.forEach(block => cy.get(`mat-grid-tile:nth-child(${block})`).click());
+  });
 
   it('Toggle id and userId!', () => {
     cy.visit('')
