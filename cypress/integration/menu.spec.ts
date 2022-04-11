a273ca5c334969e65c5d04231d5e20b8b6c9c765
DavidKf <36332661+DavidKf@users.noreply.github.com>
2022-04-10 21:28:25 -0300

    fixing cypress tests

--- cypress/integration/menu.spec.ts
+++ cypress/integration/menu.spec.ts
@@ -1,14 +1,17 @@
-describe('Toolbar', () => {
-    it('Toggle Light Mode!', () => {
+describe('Menu', () => {
+    beforeEach(() => {
         cy.visit('')
+        sessionStorage.clear();
+    });
+
+    it('Toggle Light Mode!', () => {
         cy.get('li:nth-child(1)').click()
         cy.get('mat-grid-tile:nth-child(27)').click().should('have.css', 'background-color', 'rgb(163, 216, 88)')
-    })
+    });
 
     it('Should open Github!', () => {
-        cy.visit('')
         cy.get('li:nth-child(2)').click()
         cy.url().should('include', 'https://github.com/DavidKf/post-app')
-    })
-
+        cy.visit('')
+    });
 });
